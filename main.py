7ef62aafc72663232b96fa7f2209b2b8f90120ec
1kroxi <04temka@gmail.com>
2026-01-30 22:26:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ def count_capital_letters(letter):
         if char.isupper():
             count += 1
     return count
+def decode_string(string):
+    lower = string.lower()
+    result =''
+    for char in string:
+        count = 0
+        for other_char in lower:
+            if char.lower() == other_char:
+                count +=1
+        if count == 1:
+            result += '('
+        else:
+            result += ')'
+    return result
